5d3f886dbcba652c24500461cdd056e6a4c0af49
BeforeSunset16 <zh.zhang.apply@outlook.com>
2024-10-25 17:05:45 -0400

    change to use Mantine

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,33 @@
+'use client' 
 import { Card, Image, Text } from '@mantine/core';
 import React from 'react';
 
-function Demo() {
+export default function Home() {
   return (
-    <Card
-      shadow="sm"
-      padding="xl"
-      component="a"
-      target="_blank"
-    >
-      <Card.Section>
-        <Image
-          src="/images/homeshow.png"
-          h={160}
-          alt="No way!"
-        />
-      </Card.Section>
+    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '90vh' }}>
+      <Card
+        shadow="sm"
+        padding="xl"
+        component="a"
+        target="_blank"
+        w={500}  
+      >
+        <Card.Section>
+          <Image
+            src="/images/home-show.png"
+            height={260}  // 使用 height 而不是 h
+            alt="No way!"
+          />
+        </Card.Section>
 
-      <Text fw={500} size="lg" mt="md">
-        这是未来的Idleon中文社区
-      </Text>
+        <Text fw={600} size="xl" mt="md" style={{ letterSpacing: '0.1em' }}>
+          这是未来的Idleon中文社区
+        </Text>
 
-      <Text mt="xs" c="dimmed" size="sm">
-        游戏辅助提醒工具，还有玩家攻略，游戏彩蛋，敬请期待
-      </Text>
-    </Card>
+        <Text mt="xs" c="cyan" size="sm">
+          游戏辅助提醒工具，还有玩家攻略，游戏彩蛋，敬请期待
+        </Text>
+      </Card>
+    </div>
   );
 }

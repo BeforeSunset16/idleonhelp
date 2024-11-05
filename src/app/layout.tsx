49e1140e143bf6f5bb316b15a6116f0dde834bcc
Shen <shushen2013@gmail.com>
2024-11-04 22:44:15 -0500

    feat: migrate Mantine provider and theme setup to layout.tsx, remove legacy app and document files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,16 @@
 // Import styles of packages that you've installed.
 // All packages except `@mantine/hooks` require styles imports
 import '@mantine/core/styles.css';
-import { ColorSchemeScript, MantineProvider } from '@mantine/core';
+import { ColorSchemeScript, MantineProvider, createTheme } from '@mantine/core';
 import { GoogleAnalytics } from '@next/third-parties/google';
 import './globals.css';
 import type { Metadata } from 'next';
 import type { ReactNode } from 'react';
 import HeaderTabs from './components/header';
+
+const theme = createTheme({
+  /** Put your mantine theme override here */
+});
 
 export const metadata: Metadata = {
   title: 'Idleon Help',
@@ -24,7 +28,7 @@ export default function RootLayout({
         <ColorSchemeScript />
       </head>
       <body>
-        <MantineProvider>
+        <MantineProvider theme={theme}>
           <HeaderTabs />
           {children}
           <GoogleAnalytics gaId="G-4510PJS4HL" />
